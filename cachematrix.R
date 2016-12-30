bbcc9da244c2c5ef97f603aa965ab83a7dec5a60
Bob Cooper <b.cooper@mac.com>
2016-12-30 00:49:44 -0500

    Code for class
    Added the code requested. Not sure I fully understand it but it works.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## R Programming Assignment 2
+## 2016 December
+## 
+## These function take the inverse of a matrix and cache the result making
+## it available for recall using less system resources.
+##
+## This function takes a matrix and caches in inverse of the matrix along
+## with the function environment
 
 makeCacheMatrix <- function(x = matrix()) {
+   m <- NULL 
+   set <- function(y) {
+    x <<- y
+    m <<- NULL 
+  } # function(y) end
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+} # makeCacheMatrix function end
 
-}
 
+## This function returns the inverse of the matrix previously cached
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m ## Return a matrix that is the inverse of 'x'
 }
